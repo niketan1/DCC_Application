d3d8967bd7295d884994c8c69a7a1b6c4af879d9
klvijeth <44048623+klvijeth@users.noreply.github.com>
2020-02-01 23:12:22 +0530

    Add files via upload

---
+++ server2.js
@@ -0,0 +1,67 @@
+var express= require('express');
+var app = express();
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const fs = require('fs');
+var nodemailer = require('nodemailer');
+let db = new sqlite3.Database('studentdb');
+
+app.use(express.static('static'));
+app.use(express.json());
+
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/"+"home.html")
+});
+
+app.get('/register',function(req,res){
+    console.log("register page");
+    res.sendFile(__dirname+"/"+"reg.html");
+    
+});
+
+app.get('/post_register',function(req,res){
+    console.log("post register function");
+     let sql = `insert into student values("${req.query.name}","${req.query.usn}","${req.query.gender}","${req.query.email}","${req.query.dept}",${req.query.sem})`;
+ 
+	db.all(sql, [], (err, rows) => {
+	  if (err) {
+		throw err;
+	  }
+	  rows.forEach((row) => {
+		console.log(row.name);
+	  });
+	});
+	
+	var transporter = nodemailer.createTransport({
+	  service: 'gmail',
+	  auth: {
+		user: 'klvijeth1999@gmail.com',
+		pass: 'vimala@20091999'
+	  }
+	});
+
+
+
+	var mailOptions = {
+	  from: 'klvijeth1999@gmail.com',
+	  to: req.query.email,
+	  subject: 'Registration',
+	  text: 'you have successfullly registered\nstay tuned for updates'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+	  if (error) {
+		console.log(error);
+	  } else {
+		console.log('Email sent: ' + info.response);
+	  }
+	});
+	res.status(200).send("Registration Successful");
+	
+     //console.log(response);
+});
+
+var server     =    app.listen(process.env.port,function(){
+    console.log("We have started our server on port 3000");
+});
